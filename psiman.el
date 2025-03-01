8413d007f1c8a8a28bcb84e750dea1e98180daf5
Pohlrabi <pohlrabi404@gmail.com>
2025-03-02 02:31:19 +0900

    change to accomodate other commands

--- psiman.el
+++ psiman.el
@@ -1,4 +1,4 @@
-;;; psiman.el --- A package manager for Orgmode  -*- lexical-binding: t; -*-
+;;; psiman.el --- A package manager for Emacs -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2025 Pohlrabi
 ;;
@@ -7,145 +7,139 @@
 ;; Created: February 28, 2025
 ;; Modified: February 28, 2025
 ;; Version: 0.0.1
-;; Homepage: https://github.com/pohl/pepsiman
+;; Homepage: https://github.com/pohlrabi404/psiman
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
+;; This package provides a simple way to manage system packages from within Emacs.
+;; It uses a declarative approach to manage packages by defining a list of desired
+;; packages in a file and synchronizing the system packages with this list.
 ;;
-;; Pohl's Package Manager (psiman) is a simple Emacs package designed to manage
-;; system packages using a declarative approach. It allows you to define a list
-;; of packages in a file and synchronizes the system packages with this list by
-;; installing missing packages and uninstalling packages that are no longer in
-;; the list.
+;; While the main purpose is to use it with package managers like pacman, this plugin can
+;; also be used to manage symlinks or services if provided with a way to do and undo something
 ;;
-;; Features:
-;; - Declarative Package Management: Define your desired package list in a file.
-;; - Automatic Installation and Uninstallation: Synchronize your system packages
-;;   with the declared list.
-;; - Cache Management: Maintain a cache to track installed packages.
-;;
-;; Installation:
-;; 1. Clone the repository:
-;;    git clone https://github.com/pohlrabi404/pepsiman.git
-;; 2. Load the package:
-;;    Add the following to your Emacs configuration file (`.emacs` or `init.el`):
-;;    (add-to-list 'load-path "/path/to/pepsiman")
-;;    (require 'psiman)
-;;
-;; Usage:
-;; 1. Define Package List:
-;;    Create a file at the path specified by `psiman-pkgs-path` (default: `./pkgs`).
-;;    Each line in this file should contain a package name. For example:
-;;    ```
-;;    vim
-;;    neovim
-;;    zsh
-;;    ```
-;; 2. Synchronize Packages:
-;;    Run the following command to synchronize your system packages with the
-;;    declared list:
-;;    M-x psiman-sync-all
-;;
-;; Configuration:
-;; You can customize the behavior of `psiman` by setting the following variables:
-;; - `psiman-pkgs-path`: The path to the file containing the list of packages.
-;;   Default: `./pkgs`
-;; - `psiman-cache-path`: The path to the cache file.
-;;   Default: `./cache`
-;; - `psiman-install-cmd`: The command to install packages.
-;;   Default: `"paru -S "`
-;; - `psiman-uninstall-cmd`: The command to uninstall packages.
-;;   Default: `"paru -R "`
-;;
+;; It's advised to put the path in version control and NOT put cache in version control
 ;;; Code:
 
-(defcustom psiman-pkgs-path "~/.pkgs"
-  "Package list path, psiman will use this to compare with cache."
+;; Define a customizable list of commands and paths for package management.
+(defcustom psiman-cmd-list
+  ;; Default value
+  '(("paru -S --noconfirm %s" ;; install command
+     "paru -R --noconfirm %s" ;; uninstall command
+     "~/.dotfiles/.pkg-path"              ;; pkgs path
+     "~/.config/.pkg-cache"             ;; cache path
+     " "))  ;; concat items with space
+  "A list of commands and paths for package management.
+Each sublist contains:
+- Install command
+- Uninstall command
+- Path to the package list file
+- Path to the cache file
+- Group item (used for grouping items together, put nil in if not needed and each items will have their own command)"
+  :type 'list
+  :group 'psiman)
+
+(defcustom psiman-buffer-name
+  ;; Default value
+  "*psishell*"
+  "Shell buffer name for psiman"
   :type 'string
   :group 'psiman)
 
-(defcustom psiman-cache-path "~/.cache"
-  "Cache path, psiman will use this to compare with packages list."
-  :type 'string
-  :group 'psiman)
+;; Synchronize system packages with the declared list.
+(defun psiman-sync ()
+  "Synchronize system packages with the declared list.
+This function iterates over the commands in `psiman-cmd-list` and calls
+`psiman-sync-cmd` for each set of commands."
+  (let ((temp "")
+        ;; Function to send a command to the shell.
+        (send-cmd-to-shell (lambda (cmd buffer-name)
+                             "Send a command to the shell.
+Arguments:
+- cmd: The command to send.
+- buffer-name: The name of the buffer to use for the shell."
+                             (let ((buffer (get-buffer-create buffer-name)))
+                               (switch-to-buffer buffer)
+                               (unless (eq major-mode 'shell-mode)
+                                 (shell buffer))
+                               (insert cmd)
+                               (comint-send-input)))))
+    (dolist (cmds psiman-cmd-list)
+      (setq cmd (apply 'psiman-sync-cmd cmds))
+      (if cmd
+          (if (eq temp "")
+              (setq temp (format "%s" cmd))
+            (setq temp (format "%s;%s" temp cmd)))))
+    (unless (eq temp "")
+      (funcall send-cmd-to-shell temp psiman-buffer-name))))
 
-(defcustom psiman-install-cmd "paru -S "
-  "Install command."
-  :type 'string
-  :group 'psiman)
+;; Synchronize system packages with the declared list for a specific set of commands.
+(defun psiman-sync-cmd (do-cmd undo-cmd path cache-path group-item)
+  "Synchronize system packages with the declared list for a specific set of commands.
+This function reads the package list from the file at `path`, compares it with the
+cache file at `cache-path`, and installs or uninstalls packages as necessary.
+Arguments:
+- do-cmd: The command to install packages.
+- undo-cmd: The command to uninstall packages.
+- path: The path to the package list file.
+- cache-path: The path to the cache file.
+- group-item: used for grouping items together, nil if you want to separate each commands"
+  (let* (
+         ;; Function to read a file into a list of lines.
+         (file-to-list (lambda (path)
+                         "Read a file into a list of lines.
+If the file does not exist, create an empty file at the specified path."
+                         (unless (file-exists-p path)
+                           (with-temp-file path
+                             (insert "")))
+                         (with-temp-buffer
+                           (insert-file-contents path)
+                           (split-string (buffer-string) "\n" t))))
 
-(defcustom psiman-uninstall-cmd "paru -R "
-  "Uninstall command."
-  :type 'string
-  :group 'psiman)
+         
 
-(defcustom psiman-buffer-name "*Pohl's Package Manager*"
-  "Name of the package installer buffer."
-  :type 'string
-  :group 'psiman)
+         ;; Get the list of new and old items.
+         (items-list (funcall file-to-list path))
+         (cache-list (funcall file-to-list cache-path))
+         (do-list (cl-set-difference items-list cache-list :test #'equal))
+         (undo-list (cl-set-difference cache-list items-list :test #'equal))
 
-(defun psiman-read-file-content (filepath)
-  "Read the content of a file at FILEPATH and return it as a list of strings.
-If the file does not exist, create an empty file at the specified FILEPATH."
-  (with-temp-buffer
-    (unless (file-exists-p filepath)
-      (with-temp-file filepath
-        (insert "")))
-    (insert-file-contents filepath)
-    (split-string (buffer-string) "\n" t)))
-
-(defun psiman-concat-list (list)
-  "Concatenate a LIST of strings into a single string with spaces as separators."
-  (mapconcat 'identity list " "))
-
-(defun psiman-send-cmd (command)
-  "Send a COMMAND to the shell buffer.
-If the buffer does not exist, create it and switch to shell mode."
-  (let ((buffer (get-buffer-create psiman-buffer-name)))
-    (switch-to-buffer buffer)
-    (unless (eq major-mode 'shell-mode)
-      (shell buffer))
-    (insert command)
-    (comint-send-input)))
-
-(defun psiman-get-install-list ()
-  "Get a list of packages to install based on the difference between the package list and the cache."
-  (let ((pkgs (psiman-read-file-content psiman-pkgs-path))
-        (cache (psiman-read-file-content psiman-cache-path)))
-    (psiman-concat-list (cl-set-difference pkgs cache :test #'equal))))
-
-(defun psiman-get-uninstall-list ()
-  "Get a list of packages to uninstall based on the difference between the cache and the package list."
-  (let ((pkgs (psiman-read-file-content psiman-pkgs-path))
-        (cache (psiman-read-file-content psiman-cache-path)))
-    (psiman-concat-list (cl-set-difference cache pkgs :test #'equal))))
-
-(defun psiman-do-install ()
-  "Install packages that are in the package list but not in the cache."
-  (unless (s-blank-p (psiman-get-install-list))
-    (psiman-send-cmd (format "%s %s" psiman-install-cmd (psiman-get-install-list)))))
-
-(defun psiman-do-uninstall ()
-  "Uninstall packages that are in the cache but not in the package list."
-  (unless (s-blank-p (psiman-get-uninstall-list))
-    (psiman-send-cmd (format "%s %s" psiman-uninstall-cmd (psiman-get-uninstall-list)))))
-
-(defun psiman-sync-cache ()
-  "Update the cache file with the current package list."
-  (let ((contents (with-temp-buffer
-                   (insert-file-contents psiman-pkgs-path)
-                   (buffer-string))))
-    (with-temp-file psiman-cache-path
-      (insert contents))))
-
-(defun psiman-sync-all ()
-  "Synchronize the system packages with the declared list by installing and uninstalling packages as needed."
-  (interactive)
-  (psiman-do-install)
-  (psiman-do-uninstall)
-  (psiman-sync-cache))
+         ;; Function to create the command string.
+         (make-cmd (lambda ()
+                     "Create the command string.
+If `group-item` is non-nil, group the commands together. Otherwise, create a
+separate command for each item."
+                     (if group-item
+                         (let* ((do-list-str (mapconcat 'identity do-list group-item))
+                                (undo-list-str (mapconcat 'identity undo-list group-item))
+                                (do-cmd (format do-cmd do-list-str))
+                                (undo-cmd (format undo-cmd undo-list-str)))
+                           (if do-list
+                               (if undo-list
+                                   (format "%s;%s" do-cmd undo-cmd)
+                                 (format "%s" do-cmd))
+                             (if undo-list
+                                 (format "%s" undo-cmd)
+                               '())))
+                       (let ((temp ""))
+                         (dolist (item do-list)
+                           (if (eq temp "")
+                               (setq temp (format "%s" (format do-cmd item)))
+                             (setq temp (format "%s;%s" temp (format do-cmd item)))))
+                         (dolist (item undo-list)
+                           (if (eq temp "")
+                               (setq temp (format "%s" (format undo-cmd item)))
+                             (setq temp (format "%s;%s" temp (format undo-cmd item)))))
+                         (if (eq temp "")
+                             nil
+                           (format "%s" temp)))))))
+    (setq cmd nil)
+    (if (funcall make-cmd)
+        (setq cmd (format "%s; %s" (funcall make-cmd) (format "cp %s %s;" path cache-path))))
+    cmd
+    ))
 
 (provide 'psiman)
 ;;; psiman.el ends here
